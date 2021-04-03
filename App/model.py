b75c5da08deb95852f804e986a8f43c44b9a36ef
hfbuitrago10 <hf.buitrago10@uniandes.edu.co>
2021-04-03 03:20:04 -0500

    cambios en Model

--- App/model.py
+++ App/model.py
@@ -29,91 +29,194 @@ import config as cf
 from DISClib.ADT import list as lt
 from DISClib.ADT import map as mp
 from DISClib.DataStructures import mapentry as me
-from DISClib.Algorithms.Sorting import shellsort as sa
+from DISClib.Algorithms.Sorting import mergesort as ms
 assert cf
 
 """
 Se define la estructura de un catálogo de videos. El catálogo tendrá dos listas, una para los videos, otra para las categorias de
-los mismos.
+los mismos
 """
+
+# Construcción de modelos
+
 def newCatalog():
     """
     Inicializa el catálogo de videos. Crea una lista para guardar
-    todos los videos y crea una Hash Table para guardar las category id
-    Retorna el catálogo inicializado
+    los videos. Se crean indices (maps) por los siguientes
+    criterios:
+    category
+    category ids
     """
     catalog = {'videos': None,
-                'categoryid': None}
+               'category': None, 
+               'categoryids': None}
+    
+    """
+    Esta lista contiene los videos del archivo. Los videos son
+    referenciados por los indices creados a continuación
+    """
+    catalog['videos'] = lt.newList('SINGLE_LINKED', compareVideosids)
 
-    catalog['videos'] = lt.newList(datastructure='SINGLE_LINKED',
-                                    cmpfunction=cmpVideos)
-    catalog['category_id'] = mp.newMap(80,
-                                    maptype="CHAINING",
-                                    loadfactor=4.0,
-                                    comparefunction=compareMapCategoryIds)
+    """
+    Se crean indices (maps) por diferentes criterios para llegar
+    a la información consultada
+    """
 
+    """
+    Este indice crea un map cuya llave es el category name del video
+    """
+    catalog['category'] = mp.newMap(100, 
+                                      maptype='CHAINING', 
+                                      loadfactor=4.0,
+                                      comparefunction=compareCategory)
+    
+    """
+    Este indice crea un map cuya llave es el category id del video
+    """
+    catalog['categoryids'] = mp.newMap(100,
+                                        maptype='CHAINING',
+                                        loadfactor=4.0,
+                                        comparefunction=compareCategoryids)
+    
     return catalog
-# Construccion de modelos
 
-# Funciones para agregar informacion al catalogo
+# Funciones para agregar información al catálogo
+
 def addVideo(catalog, video):
     """
-    Adiciona un video a la lista de videos
+    Adiciona un video a la lista de videos, adicionalmente, crea una entrada en el map de category ids para indicar
+    que el video pertenece a una category id específica
     """
     lt.addLast(catalog['videos'], video)
+    addCategoryids(catalog, video)
 
-def addCategory(catalog, category_dict):
+def addCategory(catalog, category):
     """
-    Añade una categoria al catalogo
+    Adiciona las categorías al map de category, donde la llave es
+    el name y el valor es el id de la categoría
     """
-    category = createCategory(category_dict['name'], category_dict['id'])
-    mp.put(catalog['category_id'], category_dict['name'], category)
+    mp.put(catalog['category'], category['name'], category['id'])
 
-# Funciones para creacion de datos
+def addCategoryids(catalog, video):
+    """
+    Adiciona un video a la lista de videos de una categoría específica,
+    las categorías se guardan en un map, donde la llave es el id de la
+    categoría y el valor es la lista de videos de esa categoría
+    """
+    try:
+        categoryids = catalog['categoryids']
+        videoCategoryid = video['category_id']
+        existcategoryid = mp.contains(categoryids, videoCategoryid)
+        if existcategoryid:
+            entry = mp.get(categoryids, videoCategoryid)
+            categoryid = me.getValue(entry)
+        else:
+            categoryid = newVideoCategory(videoCategoryid)
+            mp.put(categoryids, videoCategoryid, categoryid)
+        lt.addLast(categoryid['videos'], video)
+        categoryid['total_videos'] = lt.size(categoryid['videos'])
+    except Exception:
+        return None
 
-def createCategory(categoryId, categoryName):
+# Funciones para creación de datos
+
+def newVideoCategory(id):
     """
-    Crea una categoria para poder añadirla al mapa
+    Esta función crea la estructura de videos asociados a
+    una categoría específica
     """
-    newCategory = {"categoryname": "",
-                    "categoryid": ""}
-    newCategory["categoryname"]=categoryName
-    newCategory["categoryid"]=categoryId
-    return newCategory
+    category = {'id': '', 
+                'total_videos': 0,
+                'videos': None}
+    
+    category['id'] = id
+    category['videos'] = lt.newList()
+    return category
 
 # Funciones de consulta
-def getCategoryId(catalog, categoryname):
-    """
-    Obtiene el 'id' de una categoría por su nombre
-    """
-    for category in lt.iterator(catalog['categoryid']):
-        if categoryname in category['name']:
-            return category['id']
 
-# Funciones utilizadas para comparar elementos dentro de una lista
+def videosSize(catalog):
+    """
+    Número de videos cargados en el catálogo
+    """
+    return lt.size(catalog['videos'])
 
-def compareMapCategoryIds(key, category):
+def categorySize(catalog):
     """
-    Compara dos ids de libros, id es un identificador
-    y entry una pareja llave-valor
+    Número de categorías cargadas en el catálogo
     """
-    entry = me.getKey(category)
-    if key == entry:
+    return mp.size(catalog['category'])
+
+def getCategoryid(catalog, name):
+    """
+    Retorna el id de una categoría
+    """
+    categoryname = " " + name
+    entry = mp.get(catalog['category'], categoryname)
+    if entry:
+        categoryid = me.getValue(entry)
+        return categoryid
+    else:
+        return None
+
+def getVideosByCategory(catalog, category):
+    """
+    Retorna los videos de una categoría específica
+    """
+    category = mp.get(catalog['categoryids'], category)
+    if category:
+        return me.getValue(category)['videos']
+
+# Funciones utilizadas para comparar elementos
+
+def compareVideosids(id1, id2):
+    """
+    Compara los ids de dos videos
+    """
+    if (id1 == id2):
         return 0
-    elif key > entry:
+    elif id1 > id2:
         return 1
     else:
         return -1
 
-def cmpVideos(videotitle, video):
-    if (videotitle.lower() in video['title'].lower()):
+def compareCategory(name, category):
+    """
+    Compara los category name de dos videos, name es el 
+    nombre de la categoría y category una pareja llave-valor
+    """
+    categoryentry = me.getKey(category)
+    if (name == categoryentry):
+        return 0
+    elif (name > categoryentry):
+        return 1
+    else:
         return -1
-    return 0
+
+def compareCategoryids(id, category):
+    """
+    Compara los category ids de dos videos, id es un identificador
+    de la categoría y category una pareja llave-valor
+    """
+    categoryentry = me.getKey(category)
+    if (int(id) == int(categoryentry)):
+        return 0
+    elif (int(id) > int(categoryentry)):
+        return 1
+    else:
+        return -1
+
+def compareVideosByLikes(video1, video2):
+    """
+    Compara dos videos por su número de likes
+    """
+    return (float(video1['likes']) > float(video2['likes']))
 
 # Funciones de ordenamiento
+
 def sortVideosByLikes(catalog):
     """
-    Ordena el catálogo de videos por su número de 'likes'
+    Ordena el catálogo de videos por su número de likes
     """
-    sortVideosByLikes = ms.sort(catalog, cmpVideosByLikes)
+    sortVideosByLikes = ms.sort(catalog, compareVideosByLikes)
     return sortVideosByLikes
